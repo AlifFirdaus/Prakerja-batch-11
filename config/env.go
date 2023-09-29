fee395021772b4ee1eaeb86e68425b268cdee3b4
AlifFirdaus <78145702+AlifFirdaus@users.noreply.github.com>
2023-09-30 07:26:08 +0800

    Update env.go

--- config/env.go
+++ config/env.go
@@ -1,13 +1,11 @@
 package config
 
 import (
-	"os"
 	"github.com/joho/godotenv"
 )
 
 // LoadEnv is a function that loads environment variables from a .env file.
 func LoadEnv() {
-	os.Getenv(".env")
 	// The godotenv.Load() function loads the environment variables from the .env file.
 	// If there is an error, the function panics with the message "failed to load env file".
 	if err := godotenv.Load(); err != nil {
